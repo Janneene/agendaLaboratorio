5c15e6de06ea6a32c0917c70a2d758ff1feccd02
Janneene <porpurina_irene@hotmail.com>
2022-09-15 10:16:37 -0400

    Create cadLaboratorio.php

---
+++ laboratorio/cadLaboratorio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laboratório</title>
+	<link href="../estilos/estilo.css" rel="stylesheet">
+	<?php require '../estilos/bootstrap.php' ?>
+	<?php require '../estilos/menu.php' ?>
+</head>
+<body>
+
+</body>
+</html>
