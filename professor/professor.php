4d7654e151be17e945a29f460a11874413f51083
Janneene <porpurina_irene@hotmail.com>
2022-09-15 10:16:13 -0400

    Create professor.php
    adicionando arquivo professor

---
+++ professor/professor.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Professor</title>
+	<link href="../estilos/estilo.css" rel="stylesheet">
+	<?php require '../estilos/bootstrap.php' ?>
+	<?php require '../estilos/menu.php' ?>
+</head>
+<body>
+
+</body>
+</html>
