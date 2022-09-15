e1b872db71fedfa4a41cf5c727855d36bd0381ca
Janneene <porpurina_irene@hotmail.com>
2022-09-15 14:26:53 -0400

    Update cadLaboratorio.php

--- laboratorio/cadLaboratorio.php
+++ laboratorio/cadLaboratorio.php
@@ -10,5 +10,20 @@
 </head>
 <body>
 
+
+<div class="grid text-center">
+	<div class="g-col-6">
+		<form id="form_agendamento" autocomplete="off" name="form_agendamento" method="post" action="../validacao/recebe_form_cadLaboratorio.php">
+			<fieldset>
+       		<legend>Laboratório</legend>
+        	<label>Nome:</label><input class="campo_nome" type="text" name="nome_laboratorio" placeholder="Informática B" ><br>
+
+        	<input class="btn_submit" type="submit" value="Cadastrar" >
+    		</fieldset>
+		</form>
+	</div>
+</div>
+
+
 </body>
 </html>
