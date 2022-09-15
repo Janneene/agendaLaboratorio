c81b468a55b7a630f623263a510e96dab7a453cc
Janneene <porpurina_irene@hotmail.com>
2022-09-15 14:27:10 -0400

    Update cadProfessor.php

--- professor/cadProfessor.php
+++ professor/cadProfessor.php
@@ -9,6 +9,18 @@
 	<?php require '../estilos/menu.php' ?>
 </head>
 <body>
-
+<div class="grid text-center">
+	<div class="g-col-6">
+		<form id="form_professor" autocomplete="off" name="form_professor" method="post" action="../validacao/recebe_form_cadProfessor.php">
+			<fieldset>
+       		<legend>Professor</legend>
+        	<label>Nome:</label><input class="campo_nome" type="text" name="nome_professor" placeholder="Fulano de tal" ><br>
+        	<label>Matrícula:</label><input type="text" name="matricula" placeholder="1111111"> <br>
+        	<label>Telefone:</label><input type="text" name="telefone" placeholder="(69)99999-8888"> <br>
+        	<input class="btn_submit" type="submit" value="Cadastrar" >
+    		</fieldset>
+		</form>
+	</div>
+</div>
 </body>
 </html>
