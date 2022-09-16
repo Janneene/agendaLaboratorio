1e696943778ed3fd97652f1601d4381dcd71c5ea
Janneene <porpurina_irene@hotmail.com>
2022-09-16 16:33:01 -0400

    Update cadProfessor.php

--- professor/cadProfessor.php
+++ professor/cadProfessor.php
@@ -9,18 +9,40 @@
 	<?php require '../estilos/menu.php' ?>
 </head>
 <body>
-<div class="grid text-center">
-	<div class="g-col-6">
+
+	<div class="container">
 		<form id="form_professor" autocomplete="off" name="form_professor" method="post" action="../validacao/recebe_form_cadProfessor.php">
-			<fieldset>
-       		<legend>Professor</legend>
-        	<label>Nome:</label><input class="campo_nome" type="text" name="nome_professor" placeholder="Fulano de tal" ><br>
-        	<label>Matrícula:</label><input type="text" name="matricula" placeholder="1111111"> <br>
-        	<label>Telefone:</label><input type="text" name="telefone" placeholder="(69)99999-8888"> <br>
-        	<input class="btn_submit" type="submit" value="Cadastrar" >
-    		</fieldset>
+			<div class="grid text-center">
+				<legend>Professor</legend>
+			</div>
+			<div class="form-group">
+        		<div class="col-md-6 offset-md-3">
+        			<label>Nome:</label>
+        			<input class="form-control" type="text" name="nome_professor" placeholder="Fulano de tal" > 
+        		</div> 
+        	</div>
+        	<div class="form-group">
+        		<div class="col-md-6 offset-md-3">
+        			<label>Matrícula:</label>
+        			<input class="form-control" type="text" name="matricula" placeholder="1111111">  
+        		</div>
+        	</div>
+        	<div class="form-group">
+        		<div class="col-md-6 offset-md-3">
+        			<label>Telefone:</label>
+        			<input class="form-control" type="text" name="telefone" placeholder="(69)99999-8888">  
+        		</div>
+        	</div> <br>
+        	<div class="grid text-center">
+            	<div class="col-md-6 offset-md-3">
+                	<input type="submit" value="Cadastrar" class="btn btn-success" name="">
+            	</div>
+        	</div>
+        	
 		</form>
 	</div>
 </div>
+
+
 </body>
 </html>
